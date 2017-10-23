7377c8b38bd1f92e070fc3b31c85f7ac4a549de8
ligenfeng <ligenfeng@chinasofti.com>
2017-10-23 18:14:54 +0800

    create spring-boot project

---
+++ spring-boot/src/main/java/com/ligenfeng/controller/HelloController.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package com.ligenfeng.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author first
+ *
+ */
+@Controller
+@EnableAutoConfiguration
+public class HelloController {
+
+    @RequestMapping(value="/hello.html")
+    @ResponseBody
+    public String hello(){
+        return "Hello";
+    }
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(HelloController.class, args);
+    }
+}
