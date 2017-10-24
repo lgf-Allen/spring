5e8ce8e2836f8fcb471c5395baf3024fae73ae57
ligenfeng <ligenfeng@chinasofti.com>
2017-10-24 15:25:24 +0800

    update

--- spring-boot/src/main/java/com/allen/controller/HelloController.java
+++ spring-boot/src/main/java/com/allen/controller/HelloController.java
@@ -1,7 +1,7 @@
 /**
  * 
  */
-package com.ligenfeng.controller;
+package com.allen.controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
